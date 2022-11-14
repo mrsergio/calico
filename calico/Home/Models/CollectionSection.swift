3a8c79206dcb32a482acfc860421e93684af335f
Sergii Simakhin <sergio@mrsergio.com>
2022-11-14 22:02:24 +0700

    Optimize Home experience for iPad

--- calico/Home/Models/CollectionSection.swift
+++ calico/Home/Models/CollectionSection.swift
@@ -6,24 +6,36 @@
 //
 
 import Foundation
+import UIKit
 
 enum CollectionSectionType: Hashable {
     case banner, sliderPlain, sliderWithOverlay
     
     var sectionHeight: CGFloat {
         switch self {
-            case .banner: return 200.0
-            case .sliderPlain: return 144.0
-            case .sliderWithOverlay: return 120.0
+            case .banner: return UIDevice.current.userInterfaceIdiom == .pad ? 400.0 : 200.0
+            case .sliderPlain: return UIDevice.current.userInterfaceIdiom == .pad ? 180.0 : 144.0
+            case .sliderWithOverlay: return UIDevice.current.userInterfaceIdiom == .pad ? 144.0 : 120.0
         }
     }
     
     var itemSize: CGSize {
         // Banner item size width will be ignored since it is equal to the screen width
         switch self {
-            case .banner: return CGSize(width: 200, height: 200)
-            case .sliderPlain: return CGSize(width: 120, height: 120)
-            case .sliderWithOverlay: return CGSize(width: 180, height: 100)
+            case .banner:
+                return UIDevice.current.userInterfaceIdiom == .pad
+                ? CGSize(width: 400, height: 400)
+                : CGSize(width: 200, height: 200)
+                
+            case .sliderPlain:
+                return UIDevice.current.userInterfaceIdiom == .pad
+                ? CGSize(width: 156, height: 156)
+                : CGSize(width: 120, height: 120)
+                
+            case .sliderWithOverlay:
+                return UIDevice.current.userInterfaceIdiom == .pad
+                ? CGSize(width: 200, height: 120)
+                : CGSize(width: 180, height: 100)
         }
     }
 }
